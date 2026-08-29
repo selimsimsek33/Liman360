dfbf98680e82e4376e1eccb93b27a5cf85ef4192
selimsimsek33 <selimsimsek.33@gmail.com>
2026-08-29 11:21:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import vinext from "vinext";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
-    react(),
     vinext(),
     cloudflare({
-      viteEnvironment: { name: "ssr" },
+      viteEnvironment: {
+        name: "rsc",
+        childEnvironments: ["ssr"],
+      },
     }),
   ],
 });
